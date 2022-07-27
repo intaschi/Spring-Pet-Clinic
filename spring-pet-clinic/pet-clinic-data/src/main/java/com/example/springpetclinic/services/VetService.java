dc08d5f14060c06800cff632b92a4d4d34aa2a07
Caroline Intaschi <intaschi.caroline@gmail.com>
2022-07-27 16:09:48 -0300

    added service interfaces. close #5

---
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/example/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.example.springpetclinic.services;
+
+import com.example.springpetclinic.model.Owner;
+import com.example.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
